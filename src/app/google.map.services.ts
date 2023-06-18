483d15b3a0aaaba34428b7dd961da9f8f8004d13
ahmadharkouss <71528971+ahmadharkouss@users.noreply.github.com>
2023-06-18 20:03:56 +0200

    added possible solution

--- src/app/google.map.services.ts
+++ src/app/google.map.services.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class GoogleMapsApiService {
+
+export class GoogleMapsApiService
+{
   private apiUrl = '/api/place/details/json';
   private apiKey = 'AIzaSyAiA-lVfOcpy8DkqPEhL09ueJtCNe1vvbU';
 
   constructor(private http: HttpClient) { }
 
   getPlaceDetails(placeId: string): Observable<any> {
-    const url = `https://maps.googleapis.com/maps/api/place/details/json?fields=&place_id=${placeId}&key=${this.apiKey}`;
-    const headers = new HttpHeaders().set('Access-Control-Allow-Origin', '*');
-    return this.http.get<any>(url, { headers });
+    const url = `${this.apiUrl}?fields=&place_id=${placeId}&key=${this.apiKey}`;
+    return this.http.get<any>(url);
   }
+ 
 }
